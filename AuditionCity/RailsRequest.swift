03bd5a7c49c578ff3421ec39f133fcfea8a54592
PaulVagner <prodigyzro@gmail.com>
2015-11-25 12:21:03 -0500

    ReadMe added

--- AuditionCity/RailsRequest.swift
+++ AuditionCity/RailsRequest.swift
@@ -2,170 +2,105 @@
 //  RailsRequest.swift
 //  RR
 //
-//  Created by Susanne Burnham on 11/5/15.
-//  Copyright © 2015 Susanne Kasahara. All rights reserved.
+//  Created by Paul Vagner on 11/5/15.
+//  Copyright © 2015 Paul Vagner. All rights reserved.
 //
-
-// making a singleton to access a token
+//
+//
+// THIS WILL FUNCTION WITH ALL REQUESTS
 
 import UIKit
 
-let _rr = RailsRequest()
+private let _rr = RailsRequest()
 
-let _d = NSUserDefaults.standardUserDefaults()
-
+private let _d = NSUserDefaults.standardUserDefaults()
+/// Creates a "RailsRequest" class of type NSObject
 class RailsRequest: NSObject {
-    
-    
-    
+    /**
+     Creates a class function for the "RailsRequest"class
+     
+     - returns: the "RailsRequest"
+     */
     class func session() -> RailsRequest { return _rr }
+    
+ /// The token captured after login/register used to make authenticated API calls.
     
     var token: String? {
         
         get { return _d.objectForKey("token") as? String }
         set { _d.setObject(newValue, forKey: "token") }
         
-        
     }
+        //will need out own server info
+   
+    /// The base URL used when making API call.
+    private let APIbaseURL = "https://guarded-ridge-7410.herokuapp.com"
     
-    //// CHANGE OUT WITH TEAM 5
-    /// need methods and parameters for request, endpoint type and parameters
-    
-    
+    /**
+     This method will try to login a user with credentials below.
+     
+     - parameter username: The name used when registering.
+     - parameter password: The password used when registering.
+     */
+    func loginWithUsername(username: String, andPassword password: String) {
+        /// creates a changeable variable of "info" and sets it to "RailsRequest"
+        var info = RequestInfo()
 
-      private let base = "https://mysterious-fjord-1759.herokuapp.com"
-    
-    func loginWithUsername(username: String, andPassword password: String, completion: (loggedIn: Bool) -> ()) {
+        info.endpoint = "/users/login"
         
-        var info = RequestInfo()
-        
-        info.endpoint = "/login"
         info.method = .POST
+        
         info.parameters = [
-            
+        
             "username" : username,
-            "password" : password
-        ]
-        
-        requestWithInfo(info) { (returnedInfo) -> () in
-            
-            print(returnedInfo)
-            
-            if let user = returnedInfo?["user"] as? [String:AnyObject] {
-                
-                if let key = user["access_token"] as? String {
-                    
-                    self.token = key
-                    
-                    print(self.token)
-                    
-                    completion(loggedIn: true)
-                } else {
-                    
-                    completion(loggedIn: false)
-                    
-                }
-                
-                
-            }else {
-                
-                completion(loggedIn: false)
-                
-            }
-            
-        }
-        
-    }
-    
-    func registerWithUsername(username: String, andPassword password: String, email: String, completion: (registered: Bool) -> ()) {
-        
-        
-        var info = RequestInfo()
-    
-        info.endpoint = "/signup"
-        info.method = .POST
-        info.parameters = [
-            
-            
-            "username" : username,
-            "email" : email,
             "password" : password
             
         ]
-        
-        requestWithInfo(info) { (returnedInfo) -> () in
-            
-            
-            print(returnedInfo)
-            
-            
-            // here we grab the access token & user id in cards table view controller ,
-            
-            if let user = returnedInfo?["user"] as? [String:AnyObject] {
-                
-                if let key = user["access_token"] as? String {
-                    
-                    self.token = key
-                    
-                    print(self.token)
-                
-                    
-                    completion(registered: true)
-                } else {
-                    
-                    completion(registered: false)
-                    
-                }
-                
-                
-            }else {
-                
-                completion(registered: false)
-                
-            }
 
-            
-            
-        }
+       requiredWithInfo(info) { (returnedInfo) -> () in
         
+        if let user = returnedInfo?["user"] as? [String:AnyObject] {
+            
+            if let key = user["access_token"] as? String {
+
+                self.token = key
+            
+                }
+            }
+        
+        
+        if let errors = returnedInfo?["errors"] as? [String] {
+            //loops through errors
+            }
+    
+        }
+    
     }
     
-
-
-    // info:AnyObhect may not be the info need to casr (info: ANyObject cast for
-    func requestWithInfo(info: RequestInfo, completion: (returnedInfo: AnyObject?) -> ()) {
+    /**
+     Makes a generic request to the API, configured by the info parameter.
+     
+     - parameter info:       Used to configure the API request.
+     - parameter completion: A completion black that may be calld with an optional object.
+        The object could be an Array or Dictionary, YOU MUST handle the type within the completion black.
+     */
+    func requiredWithInfo(info: RequestInfo, completion: (returnedInfo: AnyObject?) -> ()) {
         
-        var fullURLString = base + info.endpoint
+        let fullURLString = APIbaseURL + info.endpoint
         
-        for (i,(k,v)) in info.query.enumerate() {
-            
-            if i == 0 { fullURLString += "?" } else { fullURLString += "&" }
-            
-            fullURLString += "\(k)=\(v)"
-
-        }
-    
-
-        guard let url = NSURL(string: fullURLString) else { return } // add run completion with fail
+        guard let url = NSURL(string: fullURLString) else { return } //add run completion with fail
         
         let request = NSMutableURLRequest(URL: url)
         
         request.HTTPMethod = info.method.rawValue
         
-        //add token if we have one
         
+        
+        //add token if we have one
+    
         if let token = token {
             
-            // can add token when they give it to you
-            request.setValue(token, forHTTPHeaderField: "Access-Token")
-            
-            print(token)
-            
-        }
-        
-        // add parameters to body 1. checked info parameters, turns to request data and set value for headerfield JSON string remove post data and pass in request data if weird characters show up
-        
-        if info.parameters.count > 0 {
+            request.setValue(token, forHTTPHeaderField: "Authorization")
             
             if let requestData = try? NSJSONSerialization.dataWithJSONObject(info.parameters, options: .PrettyPrinted) {
                 
@@ -173,68 +108,70 @@ class RailsRequest: NSObject {
                     
                     request.setValue("\(jsonString.length)", forHTTPHeaderField: "Content-Length")
                     
+                   
                     let postData = jsonString.dataUsingEncoding(NSASCIIStringEncoding, allowLossyConversion: true)
                     
                     request.HTTPBody = postData
                     
-                    
                 }
                 
             }
+
+            request.setValue("application/json", forHTTPHeaderField: "Content_Type")
             
+            //here we grab the access token & user id
+        
             
+        
         }
         
-        
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        
-        // creates a task from request - based on network connectivity
+        // add parameters to body
+
+        //creates a task from request
         let task = NSURLSession.sharedSession().dataTaskWithRequest(request) { (data, response, error) -> Void in
-            
-            print(data)
-            
-            // work with data returned
-            
+
+            //work with the data returned
             if let data = data {
                 
-                // have data
-                
-                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                //have data
+
+                if let returnedInfo = try? NSJSONSerialization.JSONObjectWithData(data, options: .MutableContainers) {
                     
-                    if let returnedInfo = try? NSJSONSerialization.JSONObjectWithData(data, options: .MutableContainers) {
-                        
-                        completion(returnedInfo: returnedInfo)
-                        
-                    } else {
-                        
-                        completion(returnedInfo: nil)
-                        
-                    }
+                    completion(returnedInfo: returnedInfo)
                     
-                })
-                
+                }
+
             } else {
                 
-                // no data: check for error and return alert from
+                //no data: check if error is not nil
+                
             }
+            
         }
+
+        //runs the task (aka: makes the request call)
         task.resume()
-        
     }
     
+    
 }
-
+/**
+ * A type used to collect information to build an API call.
+ */
 struct RequestInfo {
     
     enum MethodType: String {
         
         case POST, GET, DELETE
     }
-    
+    /// The part of the URL added to the base to make a specific API call.
     var endpoint: String!
+ 
+    /// The method type (GET, POST, DELETE) used to madifying the API call.
     var method: MethodType = .GET
+ 
+    /// Parameters that are required/optional to be added to the API call.
     var parameters: [String:AnyObject] = [:]
-    var query: [String:AnyObject] = [:]
     
 }
 
